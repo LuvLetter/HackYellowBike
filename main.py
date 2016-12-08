56bb009e77648164b84273906d03d8dec53a06df
Luv Letter <luvletter@Luvs-Mac-mini.local>
2016-12-08 15:55:27 +0800

    update

--- main.py
+++ main.py
@@ -3,29 +3,25 @@ import time
 from twx.botapi import TelegramBot, ReplyKeyboardMarkup
 import sqlite3
 import json
+
 def main():
     with open('bot_info.json') as bot_info:
         bot = TelegramBot(json.load(bot_info))
     updates = bot.get_updates().wait()
-    user_id = int(-301104627)
+    user_id = int(-139223745)
     mints = -1
     while True:
-        if result:
-            mints = result[0][1] + 1
-        for item in result[::-1]:
-            message = ingrex.Message(item)
-            print(u'{} {}'.format(message.time, message.text))
-            bot.send_message(user_id, u'{} {}'.format(message.time, message.text)).wait()
+        print(u'{} {}'.format(message.time, message.text))
+        bot.send_message(user_id, "start").wait()
         time.sleep(10)
 
-def getShoolID():
-    
-
-def getBikeID():
-
-def getPWD():
-
-def addPWD():
+# def getShoolID():
+#
+# def getBikeID():
+#
+# def getPWD():
+#
+# def addPWD():
 
 if __name__ == '__main__':
     main()
